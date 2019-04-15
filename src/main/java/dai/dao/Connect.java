583bf78c6d2af9493a087d525b1b0e68b578edd4
DoloresDai <841856753@qq.com>
2019-04-15 18:09:48 +0800

    Create for connecting to database.

---
+++ src/main/java/dai/dao/Connect.java
@@ -0,0 +1,64 @@
+package main.java.dai.dao;
+
+import java.sql.*;
+
+public class Connect {
+    private static final String URL = "jdbc:mysql://localhost:3306/student_examination_system";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "abc960101";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    public Connection getConnect(){
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("数据库驱动不存在！");
+        }
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL,NAME,PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("数据库连接失败！");
+        }
+        return conn;
+    }
+    public Statement getStatement(Connection conn){
+        Statement statement = null;
+        try {
+            statement = conn.createStatement();
+        } catch (SQLException e) {
+            System.out.println("创建Statement失败！");
+        }
+        return statement;
+    }
+    public ResultSet executeSQL(Statement statement,String sql){
+        ResultSet rs = null;
+        try {
+            rs = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            System.out.println("SQL语句错误！");
+        }
+        return rs;
+    }
+    public void closeConnect(ResultSet resultSet,Statement statement,Connection connection){
+        if (resultSet!=null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                System.out.println("关闭ResultSet异常!");
+            }
+        }if (statement!=null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                System.out.println("关闭Statement异常！");
+            }
+        }if (connection!=null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("关闭Connection异常！");
+            }
+        }
+    }
+}
