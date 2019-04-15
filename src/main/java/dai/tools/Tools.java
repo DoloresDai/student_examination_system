1457d6ed649512abad66914b0942f1556240ce4d
DoloresDai <841856753@qq.com>
2019-04-15 18:13:48 +0800

    Create for common code.

---
+++ src/main/java/dai/tools/Tools.java
@@ -0,0 +1,9 @@
+package main.java.dai.tools;
+
+import java.util.Scanner;
+
+public class Tools {
+    public static String getScanner(){
+        return   new Scanner(System.in).nextLine();
+    }
+}
