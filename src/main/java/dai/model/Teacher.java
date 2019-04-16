5f936d49c3f8a729167fe6b26ef5806981bab839
DoloresDai <841856753@qq.com>
2019-04-16 13:42:32 +0800

    Add subject_id attribute.

--- src/main/java/dai/model/Teacher.java
+++ src/main/java/dai/model/Teacher.java
@@ -3,6 +3,15 @@ package main.java.dai.model;
 public class Teacher {
     private int id;
     private String name;
+    private int subject_id;
+
+    public int getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(int subject_id) {
+        this.subject_id = subject_id;
+    }
 
     public int getId() {
         return id;
