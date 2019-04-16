da2ccfd8f0a87dd9d7058266164bdf593b14ce32
DoloresDai <841856753@qq.com>
2019-04-16 10:28:26 +0800

    Add sql for addService.

--- src/main/java/dai/dao/SQL.java
+++ src/main/java/dai/dao/SQL.java
@@ -1,35 +1,29 @@
 package main.java.dai.dao;
 
+import main.java.dai.model.Student;
 import main.java.dai.tools.Tools;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.stream.Stream;
-
 public class SQL {
-    public String selectFunction(String choice) {
+    public String query(String choice) {
         String[] selectChoice = choice.split("\\.");
         switch (selectChoice[0]) {
             case "1":
                 switch (selectChoice[1]) {
                     case "1":
-                        return selectStudentInfo(choice);
+                        return queryStudentInfo(choice);
                     case "2":
-                        return selectSubjectInfo(choice);
+                        return querySubjectInfo(choice);
                     case "3":
-                        return selectTeacherInfo(choice);
+                        return queryTeacherInfo(choice);
                 }
                 break;
             case "2":
-                return addStudent(choice);
+                return addInfo(choice);
         }
         return "";
     }
 
-    public String selectStudentInfo(String choice) {
+    public String queryStudentInfo(String choice) {
         String sql = "SELECT * FROM score";
         switch (choice) {
             case "1.1.1":
@@ -52,7 +46,7 @@ public class SQL {
         return sql;
     }
 
-    public String selectSubjectInfo(String choice) {
+    public String querySubjectInfo(String choice) {
         String sql = "select * from subject left join teacher on teacher.subject_id = subject.id";
         switch (choice) {
             case "1.2.1":
@@ -69,7 +63,7 @@ public class SQL {
         return sql;
     }
 
-    public String selectTeacherInfo(String choice) {
+    public String queryTeacherInfo(String choice) {
         String sql = "select * from teacher";
         switch (choice) {
             case "1.3.1":
@@ -82,32 +76,20 @@ public class SQL {
         return sql;
     }
 
-    public String addStudent(String choice) {
+    public String addInfo(String choice) {
         String sql = "insert into ";
         switch (choice) {
             case "2.1":
-                System.out.println("请输入学生信息(例如：学号：20190101，姓名：池昌旭,年龄：18,性别：男)：");
-                String[] student = Tools.getScanner().split("，");
-                ArrayList<String> studentInfo = new ArrayList<>();
-                for(String s : student){
-                    studentInfo.add(s.split("：")[1]);
-                }
-                sql = sql + " student values(" + Integer.parseInt(studentInfo.get(0)) + ",'" + studentInfo.get(1) +
-                        "'," + Integer.parseInt(studentInfo.get(2)) + ",'" + studentInfo.get(3) + "')";
-                System.out.println("添加学生成功！");
+                sql = sql +" student(id,name,age,sex) values(?,?,?,?)";
                 break;
             case "2.2":
-                System.out.println("请输入要增加的课程信息（例如：编号：2001，科目：语文，考试描述：考试内容较简单");
-                String[] subjectInfo = Tools.getScanner().split("，").toString().split("：");
-                sql = sql + " subject values(" + Integer.parseInt(subjectInfo[1]) + ",'" + subjectInfo[3] + "','" + subjectInfo[5] + "')";
-                System.out.println("添加课程信息成功！");
+                sql = sql + " subject values(id,name,describe) values(?,?,?)";
                 break;
             case "2.3":
-                System.out.println("请输入要增加的老师信息（例如：教师编号：1001，姓名：井柏然，科目：2001");
-                String[] teacherInfo = Tools.getScanner().split("，").toString().split("：");
-                sql = sql + " teacher values(" + Integer.parseInt(teacherInfo[1]) + ",'" + teacherInfo[3] + "'," + Integer.parseInt(teacherInfo[5]) + ")";
+                sql = sql + " teacher values(id,name) values(?,?,?)";
                 break;
         }
         return sql;
     }
+
 }
