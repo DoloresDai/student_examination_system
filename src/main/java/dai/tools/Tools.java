5d0f107ca054869f3d2b3adff581014f1b4a39fe
DoloresDai <841856753@qq.com>
2019-04-16 10:27:45 +0800

    Add tools for getting disassemble input.

--- src/main/java/dai/tools/Tools.java
+++ src/main/java/dai/tools/Tools.java
@@ -1,9 +1,18 @@
 package main.java.dai.tools;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tools {
     public static String getScanner(){
         return   new Scanner(System.in).nextLine();
+    }
+    public static ArrayList<String> getInfo(String s) {
+        String[] strings = s.split("，");
+        ArrayList<String> info = new ArrayList<>();
+        for (String string : strings) {
+            info.add(string.split("：")[1]);
+        }
+        return info;
     }
 }
